cd5d5e31e59f82b50ee38733aa115a75dae188c8
tizianaamica <tizianaamicarella.tag@gmail.com>
2022-04-10 19:21:13 -0400

    Created test to return movies by duration

--- java-tests/src/main/java/com/platzi/javatests/movies/service/MovieService.java
+++ java-tests/src/main/java/com/platzi/javatests/movies/service/MovieService.java
@@ -18,5 +18,9 @@ public class MovieService {
     public Collection<Movie> findMoviesByGenre(Genre genre) {
         return movieRepository.findAll().stream().filter(movie -> movie.getGenre() == genre).collect(Collectors.toList());
     }
-    
+
+    public Collection<Movie> findMoviesByLength(int length) {
+        return movieRepository.findAll().stream().filter(movie -> movie.getMinutes() <= length).collect(Collectors.toList());
+    }
+
 }
--- java-tests/src/test/java/com/platzi/javatests/movies/service/MovieServiceShould.java
+++ java-tests/src/test/java/com/platzi/javatests/movies/service/MovieServiceShould.java
@@ -3,8 +3,7 @@ package com.platzi.javatests.movies.service;
 import com.platzi.javatests.movies.model.Genre;
 import com.platzi.javatests.movies.model.Movie;
 import com.platzi.javatests.movies.repository.MovieRepository;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -14,34 +13,44 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.platzi.javatests.movies.model.Genre.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MovieServiceShould {
 
-    @Test
-    public void return_movies_by_genre() {
+    private MovieService movieService;
 
+    @Before
+    public void setUp() throws Exception {
         MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
-
         Mockito.when(movieRepository.findAll()).thenReturn(
                 Arrays.asList(
                         new Movie(1, "Dark Knigth", 152, ACTION),
                         new Movie(2, "Memento", 113, THRILLER),
                         new Movie(3, "Scream", 111, HORROR),
                         new Movie(4, "Home Alone", 103, COMEDY),
-                        new Movie(5, "Matrix", 136, ACTION),
+                        new Movie(5, "Matrix", 119, ACTION),
                         new Movie(6, "How to be single", 120, COMEDY)
                 )
         );
-
-        MovieService movieService = new MovieService(movieRepository);
-
-        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
-
-        List<Integer> moviesIds = movies.stream().map(movie -> movie.getId()).collect(Collectors.toList());
-
-        assertThat(moviesIds, is(Arrays.asList(4,6)));
-
+        movieService = new MovieService(movieRepository);
     }
+
+    @Test
+    public void return_movies_by_genre() {
+        Collection<Movie> movies = movieService.findMoviesByGenre(COMEDY);
+        assertThat(getMovieIds(movies), is(Arrays.asList(4,6)));
+    }
+
+    @Test
+    public void return_movies_by_duration() {
+        Collection<Movie> movies = movieService.findMoviesByLength(119);
+        assertThat(getMovieIds(movies), is(Arrays.asList(2, 3, 4, 5)));
+    }
+
+    private List<Integer> getMovieIds(Collection<Movie> movies) {
+        List<Integer> moviesIds = movies.stream().map(Movie::getId).collect(Collectors.toList());
+        return moviesIds;
+    }
+
 }
